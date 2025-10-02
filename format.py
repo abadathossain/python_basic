628e19dd8f4f74340c5a4c555f4ec93d41edf315
Abadat Hossain <abadat26@gmail.com>
2025-10-02 23:42:16 +0600

    format done

--- format.py
+++ format.py
@@ -1,8 +1,10 @@
-# f_name="Ab"
-# age=25
+f_name="Ab"
+age=25
 # a="Good morning"
-x="What is your name???"
-u_input=input(x)
-a="I am {}".format(u_input)
-print(u_input)
+# x="What is your name???"
+# u_input=input(x)
+z="I am {}, I am {} years old.".format(f_name,age)
+a="I am {first_name}, I am {age} years old.".format(first_name=f_name,age=age)
+# print(u_input)
+print(z)
 print(a)
