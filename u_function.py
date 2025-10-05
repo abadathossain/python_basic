e6dfebad64a20ddfcf81996e84a4b1f03f08cb29
Abadat Hossain <abadat26@gmail.com>
2025-10-05 14:55:15 +0600

    function done

---
+++ u_function.py
@@ -0,0 +1,22 @@
+def my_func():
+    a=4
+    b=5
+    print(a+b)
+my_func()
+
+
+def add(a,b):
+    print(a+b)
+
+add(5,6)
+
+def mul(a,b):
+    return a*b
+result=mul(7,8)
+print(result)
+
+
+def greetings():
+    return("Hello")
+result1=greetings()
+print(result1)
