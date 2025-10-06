78072adeb0888cdaa119057d9f7924a7cf301b27
Abadat Hossain <abadat26@gmail.com>
2025-10-06 07:39:33 +0600

    class done

---
+++ oop.py
@@ -0,0 +1,20 @@
+# class Car:
+#     def __init__(self):
+#         self.brand=""
+#         self.model=""
+
+# car1=Car()
+# car1.brand="Honda"
+# car1.model="Civic"
+# print(car1.model, car1.brand)
+
+
+class Car:
+    def __init__(self,brand, model):
+        self.brand=brand
+        self.model=model
+
+    # def __str__(self):
+    #     return f"{self.brand} {self.model}"
+car1=Car("Honda","civic")
+print(car1.brand,car1.model)
