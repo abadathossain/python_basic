e6269ab41471ac1df79b181a2c9f4da173cd13bd
Abadat Hossain <abadat26@gmail.com>
2025-10-28 08:07:57 +0600

    list comprehension done

--- comprehension.py
+++ comprehension.py
@@ -1,5 +1,15 @@
 b=[1,2,3,4,5,6]
 
+result=[]
+for i in b:
+    if i % 2==0:
+        i=i*3
+        result.append(i)      
+print(result)
+
+
+
+
 s_result=[item ** 2 for item in b if item%2==0]
 print(s_result)
 
