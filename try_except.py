3eeb74fa3e88852d7dfcc628cac11cebb41a539b
Abadat Hossain <abadat26@gmail.com>
2025-10-29 17:00:31 +0600

    try and except done

--- try_except.py
+++ try_except.py
@@ -1,5 +1,11 @@
 try:
     with open("data.csv") as f:
      print(f.read())
+     x=[1,2,3,4]
+    print(x[100])
 except FileNotFoundError:
    print("Not found")
+except IndexError:
+   print("Invalid Index")
+except Exception as e:
+   print("Custom Error",e)
